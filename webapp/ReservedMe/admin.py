65e0d24636fc1f12e47a6efc8cec19282033d11f
Tomasz-Meres <tomaszmeres@interia.pl>
2025-04-23 14:59:39 +0200

    /

--- webapp/ReservedMe/admin.py
+++ webapp/ReservedMe/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import CustomUser, Hotel, Pokoj, Rezerwacja
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+# Rejestracja niestandardowego modelu użytkownika
+admin.site.register(CustomUser, UserAdmin)
+
+# Zarejestruj inne modele
+admin.site.register(Hotel)
+admin.site.register(Pokoj)
+admin.site.register(Rezerwacja)
