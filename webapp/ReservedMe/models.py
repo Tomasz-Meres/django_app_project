a9f7669a082ad6894645c02e15718d33e01173f0
Tomasz-Meres <tomaszmeres@interia.pl>
2025-04-23 14:08:07 +0200

    .

--- webapp/ReservedMe/models.py
+++ webapp/ReservedMe/models.py
@@ -9,6 +9,7 @@ class CustomUser(AbstractUser):
     nr_tel = models.CharField(max_length=20)
 
     USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
     
     def __str__(self):
         return self.email
