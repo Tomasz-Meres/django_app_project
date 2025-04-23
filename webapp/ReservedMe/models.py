5b788997f860976227a7a4660bae5d8a458abfb8
Tomasz-Meres <tomaszmeres@interia.pl>
2025-04-23 14:06:12 +0200

    .

--- webapp/ReservedMe/models.py
+++ webapp/ReservedMe/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+# Model uzytkownika z dodatkowym polem nr_tel oraz z logowaniem za pomocą emaila
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    nr_tel = models.CharField(max_length=20)
+
+    USERNAME_FIELD = 'email'
+    
+    def __str__(self):
+        return self.email
+
