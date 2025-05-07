b8853f2b206dd831281b8576eefbd03dfffd735c
Tomasz-Meres <tomaszmeres@interia.pl>
2025-05-07 15:39:44 +0200

    .

--- webapp/ReservedMe/urls.py
+++ webapp/ReservedMe/urls.py
@@ -12,15 +12,15 @@ urlpatterns = [
     path('logowanie/', views.logowanie, name='logowanie'),  # Strona z formularzem logowania
 
     # Strony widoczne po zalogowaniu
-    path('dodaj-hotel/', views.add_hotel_view, name='add_hotel'),  # Strona 
+    path('dodaj-hotel/', views.add_hotel_view, name='add_hotel'),  # Strona z formularzem dodawania nowego hotelu
     path('zarządzaj-hotelami/', views.manage_rooms, name='manage_rooms'),  # Strona 
     path('ulubione-hotele/', views.favourite_hotels_view, name='favourite_hotels'),  # Strona 
     path('moje-rezerwacje/', views.my_reservations_view, name='my_reservations'),  # Strona 
     path('moje-dane/', views.profile_view, name='profile'),  # Strona 
-    path('moje-hotele/', views.hotel_list, name='hotel_list'),
+    path('moje-hotele/', views.hotel_list, name='hotel_list'),   # Strona z wyświetlonymi hotelami użytkownika
 
 
-    # Rejestracja oraz logowanie użytkonika
+    # Rejestracja oraz logowanie użytkownika
     path('login_user/', views.login_user, name='login_user'),  # Logowanie użytkownika
     path('logout_user/', views.logout_user, name="logout_user"),  # Wylogowanie używkownika
     path('register', views.register, name='register_user'), # Dodanie nowego użytkownika
