1c340c7cfb122689f69b8264eb8dc2718e5d5f4a
Tomasz-Meres <tomaszmeres@interia.pl>
2025-05-07 16:33:58 +0200

    .

--- webapp/ReservedMe/urls.py
+++ webapp/ReservedMe/urls.py
@@ -2,14 +2,19 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Szablony stron
+    path('base/', views.base, name='base'),  # Strona szablon
+    path('base-account', views.base_account, name='base-account'),
+
+
     # strona główna i podstrony widoczne bez logowania się
     path('', views.index, name='home'),  # Strona główna
     path('kontakt/', views.kontakt, name='kontakt'),  # Strona Kontakt
     path('o-nas/', views.o_nas, name='o_nas'),  # Strona O nas
-    path('base/', views.base, name='base'),  # Strona szablon
     path('profil/', views.profil, name='profil'),  # Strona profilu
     path('rejestracja/', views.rejestracja, name='rejestracja'),  # Strona z formularzem rejestracji
     path('logowanie/', views.logowanie, name='logowanie'),  # Strona z formularzem logowania
+
 
     # Strony widoczne po zalogowaniu
     path('dodaj-hotel/', views.add_hotel_view, name='add_hotel'),  # Strona z formularzem dodawania nowego hotelu
@@ -26,8 +31,7 @@ urlpatterns = [
     path('register', views.register, name='register_user'), # Dodanie nowego użytkownika
     path('search', views.search, name='search'), # Szukanie hoteli w bazie danych
 
-    path('base-account', views.base_account, name='base-account'),
-
+    
     # Zarządzanie hotelami i rezerwacjami
     path('create_hotel', views.add_hotel, name='create_hotel'), # dodanie nowego hotelu
 
